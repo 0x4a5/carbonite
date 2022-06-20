2d89cb6b78c85f5a620ab962773ff6b5579e7445
0x4a5 <github@crisiscoins.com>
2022-06-20 09:23:40 -0700

    created component folder structure and added user and header components

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import User from "./User";
+
+
+export default function Header() {
+  return (
+    <header className='flex justify-between p-5 text-sm text-slate-600'>
+        <div className='flex space-x-4 items-center'>
+            <p className='link'>Portfolio</p>
+            <p className='link'>Github</p>
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <p className='link'>LinkedIn</p>
+            <p className='link'>CrisisCoins</p>
+            <User/>
+        </div>
+    </header>
+  )
+}
