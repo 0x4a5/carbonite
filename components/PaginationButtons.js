07c1b08804abcd07e43cbd7eb8d8419acf7dfde0
0x4a5 <github@crisiscoins.com>
2022-06-21 14:28:53 -0700

    created and tested pagenation component

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/solid';
 
-export const PaginationButtons = () => {
+export default function PaginationButtons() {
+  const router = useRouter();
+  const startIndex = Number(router.query.start) || 1;
+  
   return (
-    <div>
-    
-    
+    <div className="flex justify-between max-w-lg text-[#0B3D6F]/100 mb-10 px-10 pb-4 sm:justify-start sm:space-x-44 sm:px-0">
+      {startIndex > 10 && (
+        <Link href={`/search?term=${router.query.term}&searchType=${
+            router.query.searchType
+          }&start=${startIndex - 10}`}
+        >
+          <div className="cursor-pointer flex flex-col items-center hover:underline">
+            <ChevronDoubleLeftIcon className="h-5" />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+      {startIndex < 90 && (
+        <Link href={`/search?term=${router.query.term}&searchType=${
+            router.query.searchType
+          }&start=${startIndex + 10}`}
+        >
+          <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+            <ChevronDoubleRightIcon className="h-5" />
+            <p>Next</p>
+          </div>
+        </Link>
+      )}
     </div>
-  )
+  );
 }
--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { PaginationButtons } from "./PaginationButtons";
 import Parser from "html-react-parser";
+import PaginationButtons from "./PaginationButtons";
 
 export default function SearchResults({ results }) {
   return (
