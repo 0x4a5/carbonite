7a50c8007715b9463af1d50a3ffb3b61e87d79f1
0x4a5 <github@crisiscoins.com>
2022-06-20 19:50:13 -0700

    implemented data abstraction from google api and created api mock data

--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,35 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
+import Response from "../Response";
 
-export default function search() {
+export default function search({ results }) {
+  console.log(results);
   return (
     <div>
-        <Head>
-            <title>Search Page</title>
-        </Head>
+      <Head>
+        <title>Search Page</title>
+      </Head>
 
-        {/* Search Header Bloc */}
-        <SearchHeader/>
+      {/* Search Header Bloc */}
+      <SearchHeader />
 
-
-        {/* Search Result Bloc */}
-
+      {/* Search Result Bloc */}
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const mockData = true
+  const data = mockData ? Response : await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${
+      process.env.CONTEXT_KEY
+    }&q=${context.query.term}${
+      context.query.searchType && "&searchType=image"
+    }&start=${startIndex}`
+  ).then((response) => response.json());
+  return {
+    props: {
+      results: data,
+    },
+  };
 }
